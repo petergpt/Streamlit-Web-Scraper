67081f17c9113da4be4f1c9dbadcf2180bcfc5c3
Peter Gostev <130558610+petergpt@users.noreply.github.com>
2023-04-13 14:52:49 +0000

    Changed the way sentiment is displayed. Resolved error with text being too long (>8k tokens)

--- helpers.py
+++ helpers.py
@@ -4,6 +4,8 @@ import re
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import json
+import nltk
+nltk.download('punkt')
 
 def format_url(url):
     parsed_url = urlparse(url)
@@ -38,11 +40,16 @@ def scrape_website(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     text = ' '.join([p.get_text() for p in soup.find_all('p')])
-    return text
+    
+    # Limit the number of tokens to 6000
+    tokens = nltk.word_tokenize(text)
+    limited_text = ' '.join(tokens[:6000])
+    
+    return limited_text
 
 def get_sentiment(text):
-    prompt = f"Please classify the sentiment of the following text - you can be as neauanced, list in 5 bullet points, keep it very short. Add in an appropriate emoji:\n\n{text}"
-    messages = [{"role": "system", "content": "You are a helpful assistant that analyzes the sentiment of text."},
+    prompt = f"Please classify the sentiment of the following text. Use emojis. Present output in bullet points:\n\n{text}"
+    messages = [{"role": "system", "content": "You are a helpful assistant that analyzes the sentiment of text and responds in emojis."},
                 {"role": "user", "content": prompt}]
     headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {os.environ["OPENAI_API_KEY"]}'}
     data = json.dumps({"model": "gpt-4", "messages": messages, "max_tokens": 100, "n": 1, "stop": None, "temperature": 0.9})
@@ -58,7 +65,7 @@ def get_sentiment(text):
 
 def get_summary(text):
     text = text.replace("'", '"')
-    prompt = f"Please provide a brief summary of the following text. Format it in bullet points. Add emojis as appropriate.:\n\n{text}"
+    prompt = f"Please provide a brief summary of the following text. Format it in bullet points. Keep it as brief as possible, limit to 5 bullet points.:\n\n{text}"
     messages = [{"role": "system", "content": "You are a helpful assistant that summarises and analyses web pages."},
                 {"role": "user", "content": prompt}]
     headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {os.environ["OPENAI_API_KEY"]}'}
